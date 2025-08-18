53872ceccad36a9f5e61e47136561e21660bbea9
khaled <khaledmohamed942@gmail.com>
2025-08-18 16:48:29 +0300

    layout for dashboard

---
+++ src/app/home/components/parts/aside-icons/aside-icons.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-icons',
+  imports: [],
+  templateUrl: './aside-icons.html',
+  styleUrl: './aside-icons.scss'
+})
+export class AsideIcons {
+
+}
